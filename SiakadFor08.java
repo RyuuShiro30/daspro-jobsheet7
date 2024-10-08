2d5cc4b58b310318b4d4e850142f25cf8524ac5c
RyuuShiro30 <annoraegaprasetyo>
2024-10-08 12:40:35 +0700

    first

--- SiakadFor08.java
+++ SiakadFor08.java
@@ -6,6 +6,7 @@ public class SiakadFor08 {
         Scanner input = new Scanner(System.in);
 
        double nilai, tertinggi = 0, terendah = 100;
+       int lulus=0, tidaklulus=0;
         
         for (int i = 1; i <= 10; i++) {
             System.out.print("masukkan nilai mahasiswa ke-" + i + ":");
@@ -18,8 +19,16 @@ public class SiakadFor08 {
                 terendah = nilai;
                 
             }
+            if (nilai >= 60) {
+                lulus++;
+                
+            } else {
+                tidaklulus++;
+            }
         }
         System.out.println("nilai tertinggi: " + tertinggi);
         System.out.println("nilai terendah: " + terendah);
+        System.out.println("jumlah mahasiswa yang lulus: " +lulus);
+        System.out.println("jumlah mahasiswa yang tida lulus: " + tidaklulus);
     }
 }
